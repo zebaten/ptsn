6656faf9dc64827fbff94a3de9d140766f3f3235
zebaten <zebatenx@gmail.com>
2023-01-03 22:44:59 -0300

    prueba web en servidor 3

--- login.php
+++ login.php
@@ -1,21 +1,22 @@
 <?php
 $usuario=$_POST['email'];
-$contraseña=$_POST['pwd'];
+$contraseña=$_POST['password'];
+echo "poto";
 session_start();
 $_SESSION['usuario']=$usuario;
 
 $conexion=mysqli_connect("mysql.face.ubiobio.cl","ptsm","ptsm2022","ptsn_bd");
 
 $consulta="SELECT * FROM Usuario where Usuario_Correo='$usuario' and Usuario_Pass='$contraseña'";
-$stmt = mysqli_prepare($conn, $consulta);
-mysqli_stmt_bind_param($stmt, $name, $email);
+$stmt = mysqli_prepare($conexion, $consulta);
+mysqli_stmt_bind_param($stmt, $usuario, $contraseña);
 $resultado = mysqli_stmt_execute($stmt);
-$filas=mysqli_fetch_array($resultado);
+$filas=mysqli_stmt_fetch($resultado);
 
 if($filas['Tipo_Usuario_Id']==1){ //administrador
     header("location:admin_home.php");
 
-}else
+}
 if($filas['Tipo_Usuario_Id']==2){ //hogar
 header("location:casa_home.php");
 }
