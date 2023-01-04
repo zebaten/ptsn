eb6d6ba59861c62e44b341e0ecd7b122df3a2010
zebaten <zebatenx@gmail.com>
2023-01-03 23:14:19 -0300

    prueba web en servidor 4

--- login.php
+++ login.php
@@ -1,16 +1,23 @@
 <?php
+error_log("Poto");
+echo "poto";
+
 $usuario=$_POST['email'];
 $contraseña=$_POST['password'];
-echo "poto";
 session_start();
 $_SESSION['usuario']=$usuario;
 
 $conexion=mysqli_connect("mysql.face.ubiobio.cl","ptsm","ptsm2022","ptsn_bd");
 
 $consulta="SELECT * FROM Usuario where Usuario_Correo='$usuario' and Usuario_Pass='$contraseña'";
+echo $consulta;
 $stmt = mysqli_prepare($conexion, $consulta);
-mysqli_stmt_bind_param($stmt, $usuario, $contraseña);
+mysqli_stmt_bind_param($stmt, 'ss', $usuario, $contraseña);
 $resultado = mysqli_stmt_execute($stmt);
+if (!$resultado) {
+    $error = mysqli_stmt_error($stmt);
+    error_log("Error executing statement: $error");
+}
 $filas=mysqli_stmt_fetch($resultado);
 
 if($filas['Tipo_Usuario_Id']==1){ //administrador
